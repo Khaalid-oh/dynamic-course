3172eb72615d97b522e9d217868e3298ecd181ff
Khalid Sulyman <84248666+Khaalid-oh@users.noreply.github.com>
2023-09-18 22:50:32 +0100

    Update CourseHeaderRecord.tsx

--- src/components/CourseHeaderRecord.tsx
+++ src/components/CourseHeaderRecord.tsx
@@ -30,4 +30,9 @@ Not a beginner per say
 
 const count = [1,2,3,4,5,6,7,8,9,10]
 
-funct
+const isPrime = (value) => {
+  for (let i = 2, s =  Math.sqrt(value), s <= i, i++){
+    if (i % 1 === 0) return false
+}
+return value > 1
+}
